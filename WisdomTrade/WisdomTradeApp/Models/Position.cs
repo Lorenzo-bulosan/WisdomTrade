091e15985b9e943a52c4bcfb2e6ce096ca49ed05
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-03-20 12:28:43 +0000

    created new classes for trade position and trade account

---
+++ WisdomTrade/WisdomTradeApp/Models/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WisdomTradeApp.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Ticker { get; set; }
+        public DateTime OpenDateTime { get; set; }
+        public DateTime CloseDateTime { get; set; }
+        public float Credit { get; set; }
+        public float UpperLimit{ get; set; }
+        public float LowerLimit { get; set; }
+        public bool Direction { get; set; }
+    }
+}

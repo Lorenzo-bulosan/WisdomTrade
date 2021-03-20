4f920ce7e4938c711b6109e0ff43a736e1cc25db
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-03-20 12:36:24 +0000

    added migrations for creating tables

---
+++ WisdomTrade/WisdomTradeApp/Data/Migrations/20210320123459_AddingPositionAndTraderAccountAndTheirJunctionTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WisdomTradeApp.Data.Migrations
+{
+    public partial class AddingPositionAndTraderAccountAndTheirJunctionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

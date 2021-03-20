e3f2e9f667ab50e630e2ca9f382cae364bd895f2
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-03-20 12:33:28 +0000

    added class for junction table

---
+++ WisdomTrade/WisdomTradeApp/Models/TraderAccount_Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WisdomTradeApp.Models
+{
+    // Junction table between TraderAccount-Position
+    public class TraderAccount_Position
+    {
+        public int PositionId { get; set; }
+        public string TraderEmail { get; set; }
+    }
+}

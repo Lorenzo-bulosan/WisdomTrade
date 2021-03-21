d6e156f432e62dd8425f09548c51732f6221c52f
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-03-21 11:24:25 +0000

    added validation for selecting dates only from today to future, and took care of time in datetime types

--- WisdomTrade/WisdomTradeApp/Models/Position.cs
+++ WisdomTrade/WisdomTradeApp/Models/Position.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CovidJournal.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +11,9 @@ namespace WisdomTradeApp.Models
     {
         public int Id { get; set; }
         public string Ticker { get; set; }
+
+        [DataType(DataType.Date)]
+        [DateLessThanEqualToday]
         public DateTime Date { get; set; }
         public float PricePrediction { get; set; }
     }
--- WisdomTrade/WisdomTradeApp/Models/WisdomTrade.cs
+++ WisdomTrade/WisdomTradeApp/Models/WisdomTrade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace WisdomTradeApp.Models
         public int Id { get; set; }
         public string Ticker { get; set; }
         public int Population { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public float FinalPricePrediction { get; set; }
 

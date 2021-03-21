5312e8cf99ecacc4a852aa5de743e95e71c4fb77
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-03-21 00:51:24 +0000

    in progress, creating report to display on index page

--- WisdomTrade/WisdomTradeApp/Controllers/HomeController.cs
+++ WisdomTrade/WisdomTradeApp/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WisdomTradeApp.Data;
 using WisdomTradeApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WisdomTradeApp.Controllers
 {
@@ -21,9 +22,38 @@ namespace WisdomTradeApp.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+
+            var allPositions = _context.Positions;
+
+            foreach(var p in allPositions)
+            {
+                Console.WriteLine(p.Date);
+
+                var dayPositions = GetAllPositionsAtDate(p.Date).AsQueryable();
+
+                foreach (var i in dayPositions)
+                {
+                    i.
+                }
+
+
+            }
+
+            return View(await _context.Positions.ToListAsync());
+        }
+
+        private float GetPriceAverage(string ticker, DateTime date)
+        {
+            var matchingPositions = _context.Positions.Where(p => p.Ticker == ticker && p.Date == date);
+
+            return matchingPositions.Sum(p => p.PricePrediction)/matchingPositions.Count(); 
+        }
+
+        private IQueryable GetAllPositionsAtDate(DateTime date)
+        {
+            return _context.Positions.Where(p => p.Date == date);
         }
 
         public IActionResult Privacy()
---
+++ WisdomTrade/WisdomTradeApp/Data/Report_AveragePricePerTickerOnSpecificDate.sql
@@ -0,0 +1,9 @@
+﻿Select
+	Ticker,
+	Date,
+	COUNT(Id) As Population,
+	AVG(PricePrediction)
+from
+	Positions
+group by
+	Ticker, Date

e8003c7f3f3e7225bc798750d2367ebbba0fc875
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-03-23 19:31:00 +0000

    migrations for deleting unused tables

---
+++ WisdomTrade/WisdomTradeApp/Data/Migrations/20210323192812_DeletedUnusedTables.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WisdomTradeApp.Data.Migrations
+{
+    public partial class DeletedUnusedTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TraderAccount");
+
+            migrationBuilder.DropTable(
+                name: "TraderAccount_Position");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TraderAccount",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Credits = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TraderAccount", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TraderAccount_Position",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PositionId = table.Column<int>(type: "int", nullable: false),
+                    TraderEmail = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TraderAccount_Position", x => x.Id);
+                });
+        }
+    }
+}
